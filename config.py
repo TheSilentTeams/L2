ecf8e086298575d0bd3a4ff07d5ae409a327b572
TheSilentTeams <shivcollegelife@gmail.com>
2025-04-19 16:21:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+
+BOT_TOKEN = "7572093305:AAE2vA99gPbAEmP_klnkPRWAZ5X5_JTklbw"
+MONGO_URI= "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority"
+API_ID = 25833520
+API_HASH = "7d012a6cbfabc2d0436d7a09d8362af7"
+SECRET_KEY = "your_super_secret"
+BIN_CHANNEL = -1002209016538
+IMG_API_KEY = "1c4e80d4ddc00be455d537ef0fc18d4a"
+OWNER_ID = 7068338513
+ALLOWED_USER_IDS = [7068338513] 
