12fd82eca630d2f6c4219df1b1316c32ebeafa92
TheSilentTeams <shivcollegelife@gmail.com>
2025-04-22 11:43:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,17 @@
 # config.py
 
-BOT_TOKEN = "7572093305:AAE2vA99gPbAEmP_klnkPRWAZ5X5_JTklbw"
+BOT_TOKENS = [
+    "7607064975:AAEwYLwvLTwdTGdDsDs7K0c9uJG323dDzg4",
+    "7572093305:AAE2vA99gPbAEmP_klnkPRWAZ5X5_JTklbw"
+]
 MONGO_URI= "mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority"
 API_ID = 25833520
 API_HASH = "7d012a6cbfabc2d0436d7a09d8362af7"
+DB_NAME = "filestream2025"
+SESSION_STRING = 'AQGGKukAHmcntxbLHdZZ99lwpnP63vaCJBg1TNflEzLTBwOaoV017Szwb4yUuN0lUpK2skf3rbTQu1m9fRGa0MNIz6g-Glw7fisjTTqE_MDUbv1nCgNM6IhX0ZCESSrzYfLimocnHJHUqso5NwX8u1QI8BFTtASquBfl1_ppsIVo2GOI71LLz0b_zTWWo-h0GIWbo3j2V3Tvn74GJQtqWMd1ovWfBO3VDMBUeLEAA7u5HX9WnY70_OmMUu2aZE9bgWrGa4OwZDpwARHA6204dobCq8Sxu-vOMtjepmHBn_uuEjKRZnmhimedEAxFoPaqIZCI-wRFUpJvnxqx0_ajG-BeurLxogAAAAF7YCQ8AA'
+DOMAIN = "http://localhost:8000"
 SECRET_KEY = "your_super_secret"
 BIN_CHANNEL = -1002209016538
 IMG_API_KEY = "1c4e80d4ddc00be455d537ef0fc18d4a"
 OWNER_ID = 7068338513
 ALLOWED_USER_IDS = [7068338513] 
-SESSION_STRING = os.environ.get("SESSION_STRING")
